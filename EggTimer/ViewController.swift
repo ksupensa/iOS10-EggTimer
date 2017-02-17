30e1e4f42e7f81a0b2c12caa42b88c6be9f540ef
supensa <stitch@mobap.local>
2017-02-18 00:51:42 +1100

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Spencer Forrest on 18/02/2017.
+//  Copyright © 2017 Spencer Forrest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
